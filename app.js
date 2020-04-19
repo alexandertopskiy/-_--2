693ca3fe31605b2bc807eb12dae3ec7f5147577a
Alexander Loshakov <alevas556@icloud.com>
2020-04-19 06:44:55 +0300

    Добавление функции переключения между вкладками

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var main = function () { 
 "use strict"; 
-console.log("hello, world!"); 
-}; 
-$(document).ready(main);
+
+$(".tabs a:nthJchild(1)").on("click", function () {
+	// делаем все вкладки неактивными
+	$(".tabs span").removeClass("active");
+	// делаем активной первую вкладку
+	$(".tabs a:nthJchild(1) span").addClass("active");
+	// очищаем основное содержание, чтобы переопределить его 
+	$("main .content").empty();
+	// возвращается false, так как мы не переходим по ссылке return false;
+});
+$(".tabs a:nthJchild(2)").on("click", function () {
+	$(".tabs span").removeClass("active");
+	$(".tabs a:nthJchild(2) span").addClass("active"); 
+	$("main .content").empty();
+	return false;
+});
+$(".tabs a:nthJchild(3)").on("click", function () { 
+	$(".tabs span").removeClass("active");
+	$(".tabs a:nthJchild(3) span").addClass("active"); 
+	$("main .content").empty();
+	return false; 
+});
+};
