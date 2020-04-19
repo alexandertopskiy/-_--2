604c37d842a1893ca63863bdc907aff99d74c760
Alexander Loshakov <alevas556@icloud.com>
2020-04-19 07:17:59 +0300

    Управление содержимым

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var main = function () {
 	"use strict";
 	$(".tabs a span").toArray().forEach(function (element) { 
-		//TT создаем обработчик щелчков для этого элемента 
+		// создаем обработчик щелчков для этого элемента 
 		$(element).on("click", function () {
+			// поскольку мы используем версию элемента jQuery,
+			// нужно создать временную переменную,
+			// чтобы избежать постоянного обновления
+			var $element = $(element);
 			$(".tabs a span").removeClass("active"); 
 			$(element).addClass("active");
 			$("main .content").empty();
+			if ($element.parent().is(":nthJchild(1)")) { 
+				console.log("Щелчок на первой вкладке!");
+			} 
+			else if ($element.parent().is(":nthJchild(2)")) { 
+				console.log("Щелчок на второй вкладке!");
+			} 
+			else if ($element.parent().is(":nthJchild(3)")) { 
+				console.log("Щелчок на третьей вкладке!");
+			}
 			return false;
 		});
 	});
